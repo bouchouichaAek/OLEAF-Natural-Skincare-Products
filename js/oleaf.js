e6245a9bfb44786887aaee44990a5857bbcf96f4
Bouchouicha Abdelkader <bouchouichaaek97@gmail.com>
2023-05-16 17:36:12 +0100

    add store page

--- js/oleaf.js
+++ js/oleaf.js
@@ -9,3 +9,18 @@ check.onclick = function () {
     check.classList.add("active");
   }
 };
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
